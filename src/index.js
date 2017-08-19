776bd7c9d509971f417ec9909d68e25691ae1393
Laura Vallhonrat <vallhonratlaura@gmail.com>
2017-08-19 12:35:52 +0200

    install packages +starting from scratch and creating first components - search api from youtube

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
+//get react and give me acces to this file import packages from node_modules
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+//api key that allows to request  youtube from google developers console website then install package youtube-api-search on terminal
+const API_KEY = 'AIzaSyBg0NBF1I7T3i4hBSkyBKpaIuw1XdBsahI';
+//import components
+import SearchBar from './components/search_bar';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//create a new component and this component should produce some html
+//es6 javascript
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>);
+}
+//take this component and put it on the page(DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
