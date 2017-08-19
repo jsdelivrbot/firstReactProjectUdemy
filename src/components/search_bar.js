445bf6d50a5e1d0aa28e6753d303d67b397e7a2f
Laura Vallhonrat <vallhonratlaura@gmail.com>
2017-08-19 17:19:26 +0200

    add search input class

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-    return <input />;
-};
+//defining new class called searchBar and give it acces to all of the functionality that react.component(added to import to refactor code) has.
+//needs to return GSX-->render()
+//every class contains render method
+class SearchBar extends Component {
+    render() {
+        return <input />;
+    }
+}
+
 
 export default SearchBar;
